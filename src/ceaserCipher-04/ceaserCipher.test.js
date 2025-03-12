2da04ad8cfd361d59b1c35140eba46fcbee6c6f9
CrispysCode <pr0ject0dinx@gmail.com>
2025-03-12 07:29:53 -0500

    wrote 3 test for ceaserCipher function

--- src/ceaserCipher-04/ceaserCipher.test.js
+++ src/ceaserCipher-04/ceaserCipher.test.js
@@ -1,3 +1,11 @@
 test('encrypts a word with mixed case', () => {
   expect(caesarCipher('heLLO', 3)).toBe('khOOR');
 });
+
+test('ensures text wrapping with large shift', () => {
+  expect(caesarCipher('xyz', 3)).toBe('abc');
+});
+
+test('preserves punctuation', () => {
+  expect(ceaserCipher('Hello, World!', 3)).toBe('Khor, Zruog!');
+});
