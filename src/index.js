f10c588d40dfbe7901c409e7e6f7ee4158718866
CrispysCode <pr0ject0dinx@gmail.com>
2025-03-12 05:39:14 -0500

    fixed plugin error when running webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,1 @@
-import { greeting } from "./firstModule.js";
 
-console.log(greeting);
--- webpack.config.js
+++ webpack.config.js
@@ -35,8 +35,13 @@ module.exports = {
       },
       {
         test: /\.jsx?$/,
-        exclude: ['node_modules'],
-        use: ['babel-loader'],
+        exclude: [path.resolve(__dirname, 'node_modules')],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       },
     ],
   },
